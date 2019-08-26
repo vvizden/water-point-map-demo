7b64fc254d7c2595dc790a182c371c2b8be42091
vviz <820853182@qq.com>
2019-08-26 09:17:08 +0800

    snapshot

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-unused-vars": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-unused-vars": process.env.NODE_ENV === "production" ? "off" : "off"
   },
   parserOptions: {
     parser: "babel-eslint"
